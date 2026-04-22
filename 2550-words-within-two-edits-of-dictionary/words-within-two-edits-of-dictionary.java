49185183bde8ffd27df9df5e6a435148239283b2
PradhaGitHub <dhivyapradhagtech12@gmail.com>
2026-04-22 09:38:10 +0530

    Time: 7 ms (55.88%) | Memory: 44.6 MB (38.97%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -4,6 +4,10 @@ class Solution {
         for(String s1:queries){
             for(String s2:dictionary){
                 int cnt=0;
+                if(s1.equals(s2)){
+                    list.add(s1);
+                    break;
+                }
                 for(int i=0;i<s1.length();i++){
                     if(s1.charAt(i)!=s2.charAt(i))cnt++;
                     if(cnt>2)break;
